dda2c4b118e8a8cf11f8ceb55fdd6ec380cd8873
yizongk <yizongk@gmail.com>
2023-01-22 19:13:07 -0500

    Add some example input and output

--- ApiGateway/main.py
+++ ApiGateway/main.py
@@ -1,22 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import asyncio
 
 app = FastAPI()
 
+"""
+Takes in
+{
+    "matchDate": "2022-12-18"
+    ,"team": "Argentina"
+    ,"result": "win"
+    ,"currency": "USD"
+    ,"amount": "100"
+    ,"email": "john.doe@gmail.com"
+}
+"""
+# @app.post("/market/bet")
+# async def placeBet(request: Request):
+#     print("Calling internal market api with:\n{}".format(await request.json()))
+#     return await request.json()
 @app.post("/market/bet")
-async def placeBet():
+def placeBet(request: Request):
+    body = asyncio.run(request.json())
+    print("Calling internal market api with:\n{}".format(body))
+    return body
+
+@app.get("/market/bet")
+async def getBet(betId):
     return {
-        "team": "Hello World!"
-        ,"": ""
+        "message": "Hello World!"
+        ,"betId": betId
         ,"": ""
         ,"": ""
         ,"": ""
     }
 
-@app.get("/market/bet")
-async def getBet():
-    return return {
+@app.get("/market/bet/{betId}")
+async def getBet(betId):
+    return {
         "message": "Hello World!"
-        ,"": ""
+        ,"betId": betId
         ,"": ""
         ,"": ""
         ,"": ""
